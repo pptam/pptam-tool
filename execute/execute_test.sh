1bb3f943f586b38ea7ddc7abb00c0f1502490e76
Andrea Janes <ajanes@unibz.it>
2020-05-03 22:39:12 +0200

    Debugging

--- execute/execute_test.sh
+++ execute/execute_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash 
-./execute_test.py --configuration ../ini2json/configuration.json $1 $2 $3 $4 $5 $6 $7 $8 $9
+
+# Make sure this command is run as sudoer, which is required for the docker commands within the script.
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+$SUDO ./execute_test.py --configuration ../ini2json/configuration.json $1 $2 $3 $4 $5 $6 $7 $8 $9
+
+
+
