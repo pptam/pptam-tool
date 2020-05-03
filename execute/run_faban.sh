462706920c2e031fee3e62c187e0a24e143dc74a
Andrea Janes <ajanes@unibz.it>
2020-05-03 22:49:34 +0200

    Debugging

--- execute/run_faban.sh
+++ execute/run_faban.sh
@@ -1,13 +1,3 @@
 #!/bin/bash 
 
 cd ./faban/master/bin/
-
-# Make sure this command is run as sudoer, which is required for the docker commands within the script.
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
-fi
-$SUDO ./startup.sh
-
-cd ../../../
-
