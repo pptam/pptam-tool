0ac09b1c4cf75822b821a5dbd24ac2e0e34568e7
Andrea Janes <ajanes@unibz.it>
2020-05-04 10:43:15 +0200

    Testing execution

--- create/create_test.py
+++ create/create_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.7
- 
+
 import os
 import shutil
 import json
@@ -7,13 +7,16 @@ import sys
 import shutil
 import argparse
 import logging
-import uuid 
+import uuid
 from datetime import datetime
 from os import path
 
+
 def replace_values_in_file(file, replacements):
-    for replacement in replacements:       
-        replace_value_in_file(file, replacement["search_for"], replacement["replace_with"])
+    for replacement in replacements:
+        replace_value_in_file(
+            file, replacement["search_for"], replacement["replace_with"])
+
 
 def replace_value_in_file(file, search_for, replace_with):
     with open(file, "r") as f:
@@ -22,9 +25,11 @@ def replace_value_in_file(file, search_for, replace_with):
     with open(file, "w") as f:
         f.write(content)
 
+
 def create_test(configuration_file_path, configuration_entries_to_overwrite):
     if not path.exists(configuration_file_path):
-        logging.fatal(f"Cannot find the configuration file {configuration_file_path}.")
+        logging.fatal(
+            f"Cannot find the configuration file {configuration_file_path}.")
         quit()
 
     with open(configuration_file_path, "r") as f:
@@ -44,30 +49,42 @@ def create_test(configuration_file_path, configuration_entries_to_overwrite):
     path_to_temp = path.join(path_to_drivers, "tmp")
 
     now = datetime.now()
-    test_id = configuration["test_case_prefix"] + "-" + now.strftime("%Y%m%d%H%M%S") + "-" + str(uuid.uuid4())[:8]
-    logging.info(f"Generating a test with the id {test_id} in {path_to_temp}.")    
-    
+    test_id = configuration["test_case_prefix"] + "-" + \
+        now.strftime("%Y%m%d%H%M%S") + "-" + str(uuid.uuid4())[:8]
+    logging.info(f"Generating a test with the id {test_id} in {path_to_temp}.")
+
     if path.isdir(path_to_temp):
         shutil.rmtree(path_to_temp)
 
-    logging.debug(f"Creating new driver, based on the templates in {path_to_templates}.")
-    shutil.copytree(path.join(path_to_templates, "faban", "driver", "ecsa"), path_to_temp)
-    shutil.copyfile(path.join(path_to_templates, "deployment_descriptor", "template", "docker-compose.yml"), path.join(path_to_temp, "deploy", "docker-compose.yml"))
-    
+    logging.debug(
+        f"Creating new driver, based on the templates in {path_to_templates}.")
+    shutil.copytree(path.join(path_to_templates, "faban",
+                              "driver", "ecsa"), path_to_temp)
+    shutil.copyfile(path.join(path_to_templates, "deployment_descriptor", "template",
+                              "docker-compose.yml"), path.join(path_to_temp, "deploy", "docker-compose.yml"))
+
     replacements = []
     for entry in configuration:
-        replacements.append({"search_for": "${" + entry.upper() + "}", "replace_with": configuration[entry]})
-        replacements.append({"search_for": "${" + entry.lower() + "}", "replace_with": configuration[entry]})
-    
-    replacements.append({"search_for": "${TEST_NAME}", "replace_with": test_id})
+        replacements.append(
+            {"search_for": "${" + entry.upper() + "}", "replace_with": configuration[entry]})
+        replacements.append(
+            {"search_for": "${" + entry.lower() + "}", "replace_with": configuration[entry]})
+
+    replacements.append(
+        {"search_for": "${TEST_NAME}", "replace_with": test_id})
 
     logging.debug(f"Replacing values.")
-    replace_values_in_file(path.join(path_to_temp, "build.properties"), replacements)
-    replace_values_in_file(path.join(path_to_temp, "deploy", "run.xml"), replacements)
-    shutil.copyfile(path.join(path_to_temp, "deploy", "run.xml"), path.join(path_to_temp, "config", "run.xml"))
-    replace_values_in_file(path.join(path_to_temp, "src", "ecsa", "driver", "WebDriver.java"), replacements)
-    replace_values_in_file(path.join(path_to_temp, "deploy", "docker-compose.yml"), replacements)
-    
+    replace_values_in_file(
+        path.join(path_to_temp, "build.properties"), replacements)
+    replace_values_in_file(
+        path.join(path_to_temp, "deploy", "run.xml"), replacements)
+    shutil.copyfile(path.join(path_to_temp, "deploy", "run.xml"),
+                    path.join(path_to_temp, "config", "run.xml"))
+    replace_values_in_file(path.join(
+        path_to_temp, "src", "ecsa", "driver", "WebDriver.java"), replacements)
+    replace_values_in_file(
+        path.join(path_to_temp, "deploy", "docker-compose.yml"), replacements)
+
     logging.info("Compiling the Faban driver")
     current_folder = os.getcwd()
     os.chdir(path_to_temp)
@@ -80,21 +97,28 @@ def create_test(configuration_file_path, configuration_entries_to_overwrite):
         quit()
 
     path_to_output = path.join(path.abspath(output), test_id)
-    logging.info(f"Writing the test case into {path_to_output}.")   
-    
+    logging.info(f"Writing the test case into {path_to_output}.")
+
     os.makedirs(path_to_output)
-    shutil.copyfile(path.join(path_to_temp, "build", f"{test_id}.jar"), path.join(path_to_output, f"{test_id}.jar"))
-    shutil.copyfile(path.join(path_to_temp, "config", "run.xml"), path.join(path_to_output, "run.xml"))
-    shutil.copyfile(path.join(path_to_temp, "deploy", "docker-compose.yml"), path.join(path_to_output, "docker-compose.yml"))
+    shutil.copyfile(path.join(path_to_temp, "build", f"{test_id}.jar"), path.join(
+        path_to_output, f"{test_id}.jar"))
+    shutil.copyfile(path.join(path_to_temp, "config", "run.xml"),
+                    path.join(path_to_output, "run.xml"))
+    shutil.copyfile(path.join(path_to_temp, "deploy", "docker-compose.yml"),
+                    path.join(path_to_output, "docker-compose.yml"))
     shutil.move(path_to_temp, path.join(path_to_drivers, test_id))
-    logging.info(f"Done.")    
-    
+    logging.info(f"Done.")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Creates test cases.")
-    parser.add_argument("--configuration", metavar="path_to_configuration_file", help="Configuration file", default="configuration.json")    
-    parser.add_argument("--logging", help="Logging level", type=int, choices=range(1, 6), default=2)    
-    parser.add_argument("--overwrite", help="Configuration values, which overwrite the values in the configuration file. Format: name1=value1 name2=value2 ...", metavar="key=value", nargs="+", default=[])    
+    parser.add_argument("--configuration", metavar="path_to_configuration_file",
+                        help="Configuration file", default="configuration.json")
+    parser.add_argument("--logging", help="Logging level",
+                        type=int, choices=range(1, 6), default=2)
+    parser.add_argument("--overwrite", help="Configuration values, which overwrite the values in the configuration file. Format: name1=value1 name2=value2 ...",
+                        metavar="key=value", nargs="+", default=[])
     args = parser.parse_args()
- 
-    logging.basicConfig(format='%(message)s', level=args.logging * 10)   
+
+    logging.basicConfig(format='%(message)s', level=args.logging * 10)
     create_test(args.configuration, args.overwrite)
--- execute/execute_test.py
+++ execute/execute_test.py
@@ -11,23 +11,36 @@ from time import sleep
 import subprocess
 import shutil
 
-def wait(seconds):
-    while seconds > 0:
-        sys.stdout.write(f"Waiting for {seconds} seconds...     \r")
-        seconds -= 1
+
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = "=" * filled_len + "-" * (bar_len - filled_len)
+    print("[%s] %s%s ...%s\r" % (bar, percents, '%', suffix), flush=True)
+
+
+def wait(seconds, suffix):
+    count = 0
+    while count < seconds:
+        progress(count, seconds, suffix)
+        count += 1
         sleep(1)
 
-def run_external_applicaton(command, information, fail_if_result_not_zero = True):
+
+def run_external_applicaton(command, information, fail_if_result_not_zero=True):
     logging.info(information)
     logging.debug(f"Executing {command}.")
     result = os.system(command)
     if fail_if_result_not_zero and result != 0:
-        logging.fatal(f"Could not execute {command}.")       
-        raise RuntimeError       
+        logging.fatal(f"Could not execute {command}.")
+        raise RuntimeError
+
 
 def execute_test(configuration_file_path):
     if not path.exists(configuration_file_path):
-        logging.fatal(f"Cannot find the configuration file {configuration_file_path}.")
+        logging.fatal(
+            f"Cannot find the configuration file {configuration_file_path}.")
         quit()
 
     with open(configuration_file_path, "r") as f:
@@ -42,37 +55,45 @@ def execute_test(configuration_file_path):
 
     output = path.abspath(configuration["test_case_executed_folder"])
     logging.debug(f"Storing results in {output}.")
-        
+
     faban_master = f"http://{configuration['faban_ip']}:9980/"
-    faban_client = path.abspath("./faban/benchflow-faban-client/target/benchflow-faban-client.jar")
+    faban_client = path.abspath(
+        "./faban/benchflow-faban-client/target/benchflow-faban-client.jar")
 
     command_to_execute_before_a_test = configuration["pre_exec_external_command"]
     command_to_execute_after_a_test = configuration["post_exec_external_command"]
     command_to_execute_at_a_test = configuration["on_exec_external_command"]
-    seconds_to_wait_for_deployment = int(configuration["test_case_waiting_for_deployment_in_seconds"])
+    seconds_to_wait_for_deployment = int(
+        configuration["test_case_waiting_for_deployment_in_seconds"])
+
+    time_to_complete_one_test = int(configuration["test_case_ramp_up_in_seconds"]) + int(
+        configuration["test_case_ramp_up_in_seconds"]) + int(configuration["test_case_ramp_up_in_seconds"])
 
     for f in os.scandir(input):
         if (path.isdir(f)):
             try:
                 logging.info(f"Executing test case {f.name}.")
-                
+
                 if (len(command_to_execute_before_a_test) > 0):
-                    run_external_applicaton(command_to_execute_before_a_test, "Running external application before the test.")
+                    run_external_applicaton(
+                        command_to_execute_before_a_test, "Running external application before the test.")
 
                 test_id = f.name
                 command_deploy_stack = f"docker stack deploy --compose-file={f.path}/docker-compose.yml {test_id}"
-                
-                run_external_applicaton(command_deploy_stack, "Deploying the system under test.") 
 
-                wait(seconds_to_wait_for_deployment)
+                run_external_applicaton(
+                    command_deploy_stack, "Deploying the system under test.")
+
+                wait(seconds_to_wait_for_deployment, "Waiting for deployment")
 
                 driver = f"{input}/{test_id}/{test_id}.jar"
                 driver_configuration = f"{input}/{test_id}/run.xml"
                 deployment_descriptor = f"{input}/{test_id}/docker-compose.yml"
                 temporary_file = f"{test_id}_run_id.tmp"
                 command_deploy_faban = f"java -jar {faban_client} {faban_master} deploy {test_id} {driver} {driver_configuration} > {temporary_file}"
-                
-                run_external_applicaton(command_deploy_faban, "Deploying the load driver.")
+
+                run_external_applicaton(
+                    command_deploy_faban, "Deploying the load driver.")
 
                 with open(temporary_file, "r") as f:
                     std_out_deploy_faban = f.readline().rstrip()
@@ -83,10 +104,13 @@ def execute_test(configuration_file_path):
                 logging.info("Waiting for the test to be completed")
                 status = ""
                 external_tool_was_started = False
+
+                time_elapsed = 0
                 while ((status != "COMPLETED") and (status != "FAILED")):
                     command_status_faban = f"java -jar {faban_client} {faban_master} status {run_id} > {temporary_file}"
 
-                    run_external_applicaton(command_status_faban, "Getting the status from Faban.")
+                    run_external_applicaton(
+                        command_status_faban, "Getting the status from Faban.")
 
                     with open(temporary_file, "r") as f:
                         std_out_status_faban = f.readline().rstrip()
@@ -96,16 +120,20 @@ def execute_test(configuration_file_path):
 
                     if (status == "STARTED" and external_tool_was_started == False and len(command_to_execute_before_a_test) > 0):
                         external_tool_was_started = True
-                        run_external_applicaton(command_to_execute_at_a_test, "Running external application with the test.")  
+                        run_external_applicaton(
+                            command_to_execute_at_a_test, "Running external application with the test.")
 
                     if ((status != "COMPLETED") and (status != "FAILED")):
-                        wait(60)
+                        wait(60, "Waiting for Faban to complete")
+                        time_elapsed = time_elapsed + 60
 
                 if (len(command_to_execute_after_a_test) > 0):
-                    run_external_applicaton(command_to_execute_after_a_test, "Running external application after the test.")
+                    run_external_applicaton(
+                        command_to_execute_after_a_test, "Running external application after the test.")
             finally:
                 command_undeploy_stack = f"docker stack rm {test_id}"
-                run_external_applicaton(command_undeploy_stack, "Undeploying the system under test.", False)
+                run_external_applicaton(
+                    command_undeploy_stack, "Undeploying the system under test.", False)
 
                 if path.isfile(temporary_file):
                     os.remove(temporary_file)
@@ -114,18 +142,24 @@ def execute_test(configuration_file_path):
                 logging.info("Saving test results")
                 test_output_path = f"{output}/{test_id}/faban"
                 os.makedirs(test_output_path)
-            
+
                 command_info_faban = f"java -jar {faban_client} {faban_master} info {run_id} > {f.name}_status.tmp"
-                shutil.copyfile(path.abspath(f"./faban/output/{run_id}/summary.xml"), test_output_path)
-                shutil.copyfile(path.abspath(f"./faban/output/{run_id}/detail.xan"), test_output_path)
-                shutil.copyfile(path.abspath(f"./faban/output/{run_id}/log.xml"), test_output_path)
+                shutil.copyfile(path.abspath(
+                    f"./faban/output/{run_id}/summary.xml"), test_output_path)
+                shutil.copyfile(path.abspath(
+                    f"./faban/output/{run_id}/detail.xan"), test_output_path)
+                shutil.copyfile(path.abspath(
+                    f"./faban/output/{run_id}/log.xml"), test_output_path)
                 shutil.move(f.path, f"{output}/{test_id}/definition")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Executes test cases.")
-    parser.add_argument("--configuration", metavar="path_to_configuration_file", help="Configuration file", default="configuration.json")    
-    parser.add_argument("--logging", help="Logging level", type=int, choices=range(1, 6), default=1)    
+    parser.add_argument("--configuration", metavar="path_to_configuration_file",
+                        help="Configuration file", default="configuration.json")
+    parser.add_argument("--logging", help="Logging level",
+                        type=int, choices=range(1, 6), default=1)
     args = parser.parse_args()
- 
-    logging.basicConfig(format='%(message)s', level=args.logging * 10)   
+
+    logging.basicConfig(format='%(message)s', level=args.logging * 10)
     execute_test(args.configuration)
