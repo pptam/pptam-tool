351a3b90c8e7db8f667c8f59c8a43a8c886805a9
Andrea Janes <ajanes@unibz.it>
2020-07-09 17:17:30 +0200

    Added train-ticket to design

--- design/locustfile.py
+++ design/locustfile.py
@@ -10,13 +10,42 @@ class TrainTicketUser(HttpUser):
     wait_time = between(5, 9)
 
     @task
-    def see_offers(self):
-        print("on_start")
-        self.client.get("/en", verify=False)
-        self.client.get("/en/offers/all", verify=False)
-        self.client.get("/en/offer/26", verify=False)
-        self.client.get("/en/offer/2", verify=False)
-        self.client.get("/en/offer/3", verify=False)
+    def search(self):
+        print("----- search")
+        self.client.get("/index.html")
+
+        trip_start = "Shang Hai"
+        trip_end = "Su Zhou"
+        trip_date = "2020-06-18T08:00:00.000"
+        data = {
+            "departureTime": trip_date,
+            "endPlace": trip_end,
+            "startingPlace": trip_start
+        }
+
+        self.client.post(url="/api/v1/travelservice/trips/left",
+                         json=data,
+                         headers={
+                             "Authorization": self.bearer,
+                             "Accept": "application/json",
+                             "Content-Type": "application/json"
+                         },
+                         name="Search")
 
     def on_start(self):
-        print("on_start")
+        print("----- on_start")
+
+        # Removes trailing / from the host name to avoid exceptions in the tests
+        self.host = self.host.rstrip("/")
+
+        response = self.client.post(url="/api/v1/users/login",
+                                    json={
+                                        "username": "fdse_microservice",
+                                        "password": "111111"
+                                    })
+
+        response_as_json = json.loads(response.content)["data"]
+        token = response_as_json["token"]
+        self.bearer = "Bearer " + token
+        self.user_id = response_as_json["userId"]
+
