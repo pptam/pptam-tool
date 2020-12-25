fed048d8da086896e946c45950ef231f2543938e
Andrea Janes <janes.andrea@gmail.com>
2020-12-25 20:58:56 +0100

    Finished modularizing pptam

--- execute/plugins/docker_stats.py
+++ execute/plugins/docker_stats.py
@@ -4,32 +4,30 @@ import threading
 import os
 import json
 import time
-
-def setup(current_configuration, output):
-    logging.debug(f"Plugin #{os.path.basename(__file__)}: setup")
     
-def before(current_configuration, output):
-    logging.debug(f"Plugin #{os.path.basename(__file__)}: before")
-
+def before(current_configuration, output, test_id):
     sut_hostname = current_configuration["docker_sut_hostname"]
     docker_client = docker.DockerClient(base_url=f"{sut_hostname}:2375")
     
     global run_docker_stats_in_background
     run_docker_stats_in_background = threading.Thread(target=get_docker_stats, args=(
+            current_configuration,
             docker_client,            
-            output,
-            True
+            output
         ), daemon=True)
     run_docker_stats_in_background.start()
 
-def get_docker_stats(client, output_path, verbose = False):
+def get_docker_stats(current_configuration, client, output_path):
+    sleep_between_stats_reading_in_seconds = int(current_configuration["docker_stats_sleep_between_stats_reading_in_seconds"])
+    is_verbose = current_configuration["docker_stats_verbose"]=="1"
+    
     while True:
         with open(os.path.join(output_path, "docker_stats.log"), "a") as f:
-            if not verbose:
+            if not is_verbose:
                 f.write("timestamp, container, cpu_usage, memory_usage, memory_limit\n")
             for container in client.containers.list():
                 stats = container.stats(stream=False) # takes about 2s
-                if not verbose:
+                if not is_verbose:
                     timestamp = stats["read"]
                     container = container.name
                     cpu_usage = stats["cpu_stats"]["cpu_usage"]["total_usage"]
@@ -39,8 +37,4 @@ def get_docker_stats(client, output_path, verbose = False):
                 else:
                     f.write(json.dumps(stats) + '\n')
 
-        time.sleep(10)  # Configure
-
-
-def shutdown(current_configuration, output):
-    logging.debug(f"Plugin #{os.path.basename(__file__)}: shutdown")
+        time.sleep(sleep_between_stats_reading_in_seconds) 
---
+++ execute/plugins/locust.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from lib import run_external_applicaton
+
+def get_configuration_files(current_configuration, output, test_id):
+    return ["locustfile.py"]
+
+def run(current_configuration, output, test_id):
+    driver = f"{output}/locustfile.py"
+    host = current_configuration["locust_host_url"]
+    load = current_configuration["load"]
+    spawn_rate = current_configuration["spawn_rate_per_second"]
+    run_time = current_configuration["run_time_in_seconds"]
+    log_file = os.path.splitext(driver)[0] + ".log"
+    out_file = os.path.splitext(driver)[0] + ".out"
+    csv_prefix = os.path.join(os.path.dirname(driver), "result")
+    logging.info(f"Running the load test for {test_id}, with {load} users, running for {run_time} seconds.")
+    run_external_applicaton(
+        f'locust --locustfile {driver} --host {host} --users {load} --spawn-rate {spawn_rate} --run-time {run_time}s --headless --only-summary --csv {csv_prefix} --csv-full-history --logfile "{log_file}" --loglevel DEBUG >> {out_file} 2> {out_file}', False)
+
+
