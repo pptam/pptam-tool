ecae2e3916b2b0e65af2e8136991261bff1a5dba
Andrea Janes <janes.andrea@gmail.com>
2021-01-13 17:59:20 +0100

    l

--- installation/scripts/get_pptam.sh
+++ installation/scripts/get_pptam.sh
@@ -10,21 +10,13 @@ echo "                                                      |_|    ";
 
 cd ~
 
-# Install InfluxDB
-wget https://dl.influxdata.com/influxdb/releases/influxdb_2.0.0-beta.16_linux_amd64.tar.gz
-tar xvfz influxdb_2.0.0-beta.16_linux_amd64.tar.gz
-mv influxdb_2.0.0-beta.16_linux_amd64 influxdb
-./influxdb/influxd &
-sleep 10
-./influxdb/influx setup --bucket pptam --force --host http://localhost:9999 --name pptam --org pptam --password 12345678 --retention 0 --token pptam --username pptam
-
 # Install Grafana
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana -y
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get install -y software-properties-common wget
+# wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+# echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# sudo apt-get update
+# sudo apt-get install grafana -y
 
 # Cloning PPTAM from github
 sudo rm -rf pptam-tool
