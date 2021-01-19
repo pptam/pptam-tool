4465e4d2ca3b347180c4d5486a3eacd286838ef0
Janes Andrea <ajanes@unibz.it>
2021-01-19 17:54:41 +0100

    Added exception handling to Jaeger

--- execute/plugins/measure_jaeger.py
+++ execute/plugins/measure_jaeger.py
@@ -4,23 +4,27 @@ import requests
 import json
 
 def after(global_plugin_state, current_configuration, output, test_id): 
-    session = requests.Session()
-    session.headers.update({'accept': 'application/json'})
-    session.headers.update({'content-type': 'application/json'})
+    try: 
+        session = requests.Session()
+        session.headers.update({'accept': 'application/json'})
+        session.headers.update({'content-type': 'application/json'})
 
-    jaeger_host = current_configuration["jaeger_host_url"]
-    jaeger_services = current_configuration["jaeger_services"].split(" ")
-    
-    service_request = session.get(f"{jaeger_host}/api/services")
-    all_services = json.loads(service_request.content)["data"]
+        jaeger_host = current_configuration["jaeger_host_url"]
+        logging.info(f"Contacting Jaeger host {jaeger_host}/api/services")
+        jaeger_services = current_configuration["jaeger_services"].split(" ")
+        
+        service_request = session.get(f"{jaeger_host}/api/services")
+        all_services = json.loads(service_request.content)["data"]
 
-    for service in all_services:
-        if (any("all" in s for s in jaeger_services) or any(service in s for s in jaeger_services)):
-            file_to_write = os.path.join(output, f"jaeger_{service}.log")  
+        for service in all_services:
+            if (any("all" in s for s in jaeger_services) or any(service in s for s in jaeger_services)):
+                file_to_write = os.path.join(output, f"jaeger_{service}.log")  
 
-            logging.info(f"Wrinting Jaeger data for service {service}.")
-            with open(file_to_write, "a") as f:
-                request = session.get(f"{jaeger_host}/api/traces?service=" + service)
-                data = json.loads(request.content)["data"]
-                f.write(json.dumps(data, indent=2))
+                logging.info(f"Wrinting Jaeger data for service {service}.")
+                with open(file_to_write, "a") as f:
+                    request = session.get(f"{jaeger_host}/api/traces?service=" + service)
+                    data = json.loads(request.content)["data"]
+                    f.write(json.dumps(data, indent=2))
             
+    except Exception as e:
+        self.logging.critical(f"Exception in Jaeger: {e}")   
