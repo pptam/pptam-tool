f13290f35b8a8d7c8eb078e99e8de8a46402dd21
Janes Andrea <ajanes@unibz.it>
2021-04-19 17:06:38 +0200

    Refactoring installation

--- installation/scripts/install_driver.sh
+++ installation/scripts/install_driver.sh
@@ -12,6 +12,7 @@ echo "                                                  |_|    ";
 # Docker swarm installation and writing join token into file
 docker swarm init --advertise-addr $1 --listen-addr $1
 docker swarm join-token -q worker > /vagrant/.join-token-worker
+sudo apt install docker-compose
 
 # Python installation
 apt install -y python3.8 python3.8-dev python3-pip postgresql postgresql-contrib libpq-dev
@@ -25,7 +26,17 @@ sudo apt-get update
 sudo apt-get install git -y
 
 # Setup Postgres
-sudo -u postgres psql --command "ALTER USER postgres WITH PASSWORD 'password';"
+sudo /etc/init.d/postgres start
+
+cd ~
+git clone --depth 1 https://github.com/pptam/pptam-tool.git
+
+git clone --depth 1 https://github.com/pptam/pptam-data.git
+sudo -u postgres psql --command "ALTER USER postgres WITH PASSWORD 'postgres';"
+sudo -u postgres psql --command "CREATE DATABASE pptam;"
+sudo -u postgres psql --dbname=pptam --file ~/pptam-data/init.sql >/dev/null 
+sudo update-rc.d postgresql enable
+sudo rm -rf ~/pptam-data
 
 # Create Docker images
 cd /vagrant/scripts/docker/
