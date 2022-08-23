9b20b89ea045cbf7b93a6ab9c52071707b092ef3
Andrea Janes <ajanes@unibz.it>
2022-08-23 14:02:03 +0200

    Update

--- toolchain/store_all_experiments.sh
+++ toolchain/store_all_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd ./executed
-for n in *; do printf './store.py ./executed/%s\n' "$n"; done > ../store_to_do.sh
+for n in *; do printf './store.py --skip-history ./executed/%s\n' "$n"; done > ../store_to_do.sh
 chmod uog+x ../store_to_do.sh
 cd ..
 bash ./store_to_do.sh
