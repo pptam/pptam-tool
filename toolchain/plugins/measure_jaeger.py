f929e18ced27c23ca95a4e579152661553672b63
Andrea Janes <janes.andrea@gmail.com>
2025-03-17 14:18:56 +0100

    Update

--- toolchain/plugins/measure_jaeger.py
+++ toolchain/plugins/measure_jaeger.py
@@ -5,6 +5,7 @@ import json
 import os
 import pandas as pd
 from datetime import datetime, timedelta, timezone
+import pygraphviz as pgv
 
 def fetch_services(jaeger_api_url):
     response = requests.get(f"{jaeger_api_url}/services")
@@ -60,6 +61,18 @@ def save_dag_as_csv(output, G):
     df.to_csv(file_to_write, index=False)
 
 
+def save_dag_as_pdf(output, G):
+    edges = [(u, v, G[u][v].get("weight", 1)) for u, v in G.edges]
+    df = pd.DataFrame(edges, columns=["Parent", "Child", "Weight"])
+
+    graph = pgv.AGraph(directed=True)
+    for _, row in df.iterrows():
+        graph.add_edge(row['Parent'], row['Child'], label=row['Weight'])
+    
+    graph.layout(prog='dot')  # Use dot layout for hierarchy
+    file_to_write = os.path.join(output, "dag.pdf")
+    graph.draw(file_to_write)
+
 def after(current_configuration, design_path, output, test_id):
     jaeger_api_url = current_configuration["jaeger_api_url"]
     seconds_to_wait_before_after = int(current_configuration["seconds_to_wait_before_after"])
@@ -67,11 +80,6 @@ def after(current_configuration, design_path, output, test_id):
     run_time = run_time_in_seconds + seconds_to_wait_before_after
 
     try:
-        dependencies_data = fetch_dependencies(jaeger_api_url)
-        dag = build_dag(dependencies_data)
-        save_dag_as_json(output, dag)
-        save_dag_as_csv(output, dag)
-
         services = fetch_services(jaeger_api_url)
         
         for service in services:
@@ -84,5 +92,12 @@ def after(current_configuration, design_path, output, test_id):
             except requests.exceptions.RequestException as e:
                 logging.error(f"Error fetching traces for service {service}: {e}")
                 
+        dependencies_data = fetch_dependencies(jaeger_api_url)
+        dag = build_dag(dependencies_data)
+        save_dag_as_json(output, dag)
+        save_dag_as_csv(output, dag)
+        save_dag_as_pdf(output, dag)
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching data from Jaeger: {e}")
+
+    
